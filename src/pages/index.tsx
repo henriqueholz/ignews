ad7b441c60e7a0fb7fecff2df3edcde8d7ee5946
henriqueholz <henriqueholz@live.com>
2022-05-18 22:03:52 -0300

    Creating home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-import Head from "next/head"
+import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
   <>
     <Head>
-      <title>ig.news</title>
+      <title>Home | ig.news</title>
     </Head>
+
+    <main className={styles.hero}>
+      <section>
+        <span>
+          Hey, welcome
+        </span>
+        <h1> News about the <span>React</span> world</h1>
+        <p>
+          Get access to all the publications <br />
+        <span>for $9.99 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding"/>
+    </main>
   </>
   )
 }
