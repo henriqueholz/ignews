1ced4b87e797bdfc09a2900917b8e487f42369b9
henriqueholz <henriqueholz@live.com>
2022-05-18 22:22:39 -0300

    Creating Subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
 export default function Home() {
@@ -18,6 +19,7 @@ export default function Home() {
           Get access to all the publications <br />
         <span>for $9.99 month</span>
         </p>
+        <SubscribeButton/>
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding"/>
