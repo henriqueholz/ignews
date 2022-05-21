0c446750159e432014194955d690cdc2e8352d7d
henriqueholz <henriqueholz@live.com>
2022-05-20 23:32:59 -0300

    Adding stripe API

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from "./styles.module.scss";
 
-export function SubscribeButton() {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   return (
     <button
     type="button"
@@ -9,3 +13,4 @@ export function SubscribeButton() {
     </button>
   )
 }
+
