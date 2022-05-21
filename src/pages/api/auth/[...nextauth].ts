d712f857d946d13a1201c7fd240e581a48ed080a
henriqueholz <henriqueholz@live.com>
2022-05-21 15:48:07 -0300

    Saving logged user in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,36 @@
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 
+import { query as q } from "faunadb"
+
+import { fauna } from "../../../services/fauna"
+
 export default NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user, user:email"
+        }
+      }
     })
-  ]
+  ], 
+  callbacks: {
+    async signIn({user, account, profile}) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection("users"),
+            { data: { email }}
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 })
